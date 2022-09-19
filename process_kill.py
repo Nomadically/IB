6c321db943a2518fed12f53a636b7e31572dd4a4
Nomadically <phonely0039@gmail.com>
2022-09-19 15:33:00 -0400

    9/19/22: further updates

--- process_kill.py
+++ process_kill.py
@@ -1,27 +1,44 @@
+import unicodedata
+import html5lib
 import keyboard
-import psutil
+import requests
+import json
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+#
+html_doc = requests.get('https://www.bop.gov/locations/institutions/tha/#send_things')
+soup = BeautifulSoup(html_doc.content, 'html.parser')
+
+holding = []
+tag = soup.br
+#f = codecs.open('temptext.txt', encoding='utf-8',mode='w+')
+for string in tag.strings:
+    print(string)
+
+#     holding.append(unicodedata.normalize("NFKD", string))
+#
+# index = holding.index('INMATE NAME & REGISTER NUMBER')
+#
+# print(holding)
+#
+# print(len(holding))
+# print(index)
+#
+# print(holding[366])
+#
 
 
-
-from ahk import AHK
-ahk = AHK()
-
-win_main = ahk.win_get(title='Channergy 2021 Client/Server')
-
-win_main.activate()
-keyboard.send('f4, esc, alt+n')
-# keyboard.send('esc')
-# keyboard.send('alt+n')
-
-
-
-
-
-
-
-
-
-
+# from ahk import AHK
+# ahk = AHK()
+#
+# win_main = ahk.win_get(title='Channergy 2021 Client/Server')
+#
+# win_main.activate()
+# keyboard.send('f4, esc, alt+n')
+# # keyboard.send('esc')
+# # keyboard.send('alt+n')
+#
 
 
 # def main():
@@ -38,4 +55,18 @@ keyboard.send('f4, esc, alt+n')
 #     main()
 
 
+"""
 
+url = "https://www.bop.gov/locations/institutions/tha/"
+page = urlopen(url).read()
+soup = bs(page, 'html.parser')
+
+for item in soup.find_all('div'):
+    if 'For inmates at the ' in item.text:
+        data = item.text.split('var productDetail =')[-1].split('};')[0] + "}"
+        datajson = json.loads(data.strip())
+        print('Product Code :' + datajson['ProductCode'])
+        for item in datajson['ProductSpecification']:
+            print(item['SpecificationName'] + " : " + item['SpecificationValue'])
+
+"""
--- selenium_test.py
+++ selenium_test.py
@@ -1,32 +1,42 @@
+import urllib
+import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+from bs4 import BeautifulSoup
+import re
 
 driver = webdriver.Chrome("C:\\Users\\ib\\PycharmProjects\\ibProject\\chromedriver.exe")
 driver.get("https://www.bop.gov/inmateloc/")
-print(driver.title)
+print("this is title", driver.title)
 time.sleep(1) # Let the user actually see something!
 
 # print(driver.page_source)
 
-inmate_number = 41138018
+# inmate_number = 41138018
+inmate_number = "14083026"
 
 element = driver.find_element(By.XPATH, "/html/body/div[1]/div/section/div/div[2]/div[1]/table/tbody/tr[2]/td[3]/input")
 element.send_keys(inmate_number)
 element.send_keys(Keys.RETURN)
 time.sleep(1)
 
-result = driver.find_element(By.XPATH, "/html/body/div[1]/div/section/div/div[2]/div[1]/div[2]/div[2]/table/tbody/tr[6]/td/a")
+result = driver.find_element(By.XPATH, "/html/body/div[1]/div/section/div/div[2]/div[1]/div[2]/div[2]/table/tbody/tr[6]/td/a").click()
 
-print(result.click())
+html_doc = requests.get('https://www.bop.gov/locations/institutions/tha/#send_things')
+soup = BeautifulSoup(html_doc.content, 'html.parser')
 
-facility_name = driver.find_element(By.CLASS_NAME, "send-address-title")
-# facility_address_street = driver.find_element
-# facility_address_city = driver.find_element
-# facility_address_state = driver.find_element
-# facility_address_zip = driver.find_element
+holding = []
+#f = codecs.open('temptext.txt', encoding='utf-8',mode='w+')
+for string in soup.strings:
+    holding.append(string)
 
-print(
-    facility_name
-)
+index = holding.index('INMATE NAME & REGISTER NUMBER')
+
+print(index)
+
+print(holding[364])
+
+time.sleep(3)
+# driver.quit()
